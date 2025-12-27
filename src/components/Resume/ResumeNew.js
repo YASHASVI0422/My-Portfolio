7cae9916a6fef6de889f71758488e39718ddc262
Yashasvi Pandey <127979114+YASHASVI0422@users.noreply.github.com>
2025-12-28 00:23:27 +0530

    Fix resume PDF preview scrolling

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,26 +1,49 @@
 import React from "react";
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 import { AiOutlineDownload } from "react-icons/ai";
 
-// Make sure your resume is inside: public/resume.pdf
 function ResumeNew() {
   return (
     <Container fluid className="resume-section">
       <Particle />
 
-      <Row style={{ justifyContent: "center", position: "relative" }}>
+      {/* Download Button */}
+      <Row className="justify-content-center mb-4">
         <Button
           variant="primary"
-          href="/resume.pdf"
+          href="/Yashasvi_Resume.pdf"
           target="_blank"
           rel="noreferrer"
-          style={{ maxWidth: "250px" }}
         >
           <AiOutlineDownload />
           &nbsp;Download CV
         </Button>
+      </Row>
+
+      {/* PDF Preview */}
+      <Row className="justify-content-center">
+        <Col md={8}>
+          <div
+            style={{
+              height: "90vh",
+              overflowY: "auto",
+              borderRadius: "8px",
+              boxShadow: "0 0 10px rgba(0,0,0,0.3)",
+            }}
+          >
+            <iframe
+              src="/Yashasvi_Resume.pdf"
+              title="Resume PDF"
+              width="100%"
+              height="100%"
+              style={{
+                border: "none",
+              }}
+            />
+          </div>
+        </Col>
       </Row>
     </Container>
   );
